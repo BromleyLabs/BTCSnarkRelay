d1597f2789acc8c7bb176dd0a5348a4b39beb8a1
Puneet Joshi <puneet@outdu.com>
2018-08-28 14:28:01 +0530

    Added.

---
+++ test/read_hash.py
@@ -0,0 +1,24 @@
+# Script to present a hash value in hex from output of SNARK output generator 
+# The output should be saved in a file with {} around start and end so that
+# this python script reads it as a dictionary. Example:
+# {"~out_0": 1, "~out_1": 1, "~out_10": 0, "~out_100": 0, "~out_101": 0, "~out_102": 1, "~out_103": 0, ...... }
+
+import sys
+from bitstring import BitArray
+
+def main():
+    if len(sys.argv) != 2:
+       print('Usage: python read_hash.py <filename>')
+       exit(0) 
+
+    s = open(sys.argv[1], 'rt').read()   
+    d = eval(s)
+    out = []
+    for i in range(255, -1, -1):
+        k = '~out_%d' % i
+        out.append(d[k])
+
+    print(BitArray(out))
+
+if __name__== '__main__':
+    main()
