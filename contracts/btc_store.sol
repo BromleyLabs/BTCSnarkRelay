9c02286c352c021b1c85bc0c3d181ed4228a5a09
Bon Filey <bonfiley@gmail.com>
2018-09-10 10:48:46 +0530

    Last Difficulty Adjust Time verified. WIP.

--- contracts/btc_store.sol
+++ contracts/btc_store.sol
@@ -21,6 +21,7 @@ contract BTCHeaderStore {
     }
 
     uint m_last_verified_block = 0;
+    uint m_init_diff_adjust_time; /* Block time when difficulty was adjusted */
    
     /* block_number => HeaderInfo map */
     mapping (uint => HeaderInfo) public m_headers; 
@@ -48,6 +49,20 @@ contract BTCHeaderStore {
         return BytesLib.toUint(b, 0); 
     }
 
+    /**
+     * @dev Utility function to extract BTC block time. Block time is stored in 
+     * 4 bytes. 28 bytes are padded to make it 32 bytes and then converted to
+     * uint.  The function will raise exception if block number provided as 
+     * arugment is not available.
+     */
+
+    function get_block_time(uint block_number) internal view returns (uint) { 
+        bytes memory block_bytes = m_headers[block_number].data;     
+        bytes memory time_bytes = BytesLib.slice(block_bytes, 68, 4);
+        bytes memory padding = new bytes(28); /* To make it 32 bytes */ 
+        time_bytes = BytesLib.concat(padding, time_bytes);
+        return BytesLib.toUint(time_bytes, 0); 
+    }
 
     /*
      * @dev One time setting of contract that is going to call verify()
@@ -63,11 +78,13 @@ contract BTCHeaderStore {
      * This is one-time setting. 
      * @param block_number assumed > 0
      */
-    function store_start_block_header(bytes data, uint block_number) public {
+    function store_start_block_header(bytes data, uint block_number, 
+                                      uint diff_adjust_time) public {
         require(m_last_verified_block == 0);
         require(block_number > 0);
         m_headers[block_number] = HeaderInfo(data, true); 
         m_last_verified_block = block_number;
+        m_init_diff_adjust_time = diff_adjust_time;
     }
 
     /**
@@ -81,16 +98,28 @@ contract BTCHeaderStore {
         m_headers[block_number] = HeaderInfo(data, false); 
     } 
 
+    function get_last_diff_adjust_time(uint last_verified_block) internal
+                                       view returns (uint) {
+        int last_diff_adjust_block = int(last_verified_block) - 
+                                     int((last_verified_block % 2016));
+        if (last_diff_adjust_block < 0) 
+            return m_init_diff_adjust_time;
+
+        return get_block_time(uint(last_diff_adjust_block));
+    }
+
     /**
      * @dev Verify a group of headers. This method can only be called by 
      * by SNARK verifier contract. 
+     * @param last_diff_adjust_time Time of the block when difficulty was
+     * adjusted.
      * @param hash248 Int of lower 248 bits of hash of last verified block
      * @param concatHash248 Int of lower 248 bits of 
      * hash(concat(hash of headers to be verified)
      * @param n_headers Number of headers to be verified. The headers are
      * ordered - latest first
      */
-    function verify(uint last_verified_block, uint hash248, uint concatHash248,
+    function verify(uint last_diff_adjust_time, uint last_verified_block, uint hash248, uint concatHash248,
                     uint n_headers) public returns (bool) {
         require(m_verifier_addr != address(0));
         require(msg.sender == m_verifier_addr);
@@ -110,6 +139,8 @@ contract BTCHeaderStore {
         }
         bytes32 concatHash = sha256(concat_headers); 
         require(concatHash248 == hash_to_uint248(concatHash));
+
+        require(get_last_diff_adjust_time(last_verified_block) == last_diff_adjust_time); 
 
         for (i = 0; i < n_headers; i++) 
             m_headers[m_last_verified_block + i + 1].verified = true;
